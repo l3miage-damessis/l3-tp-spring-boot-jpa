03efb2e7482be151498e114bf145ef33df5878d0
l3miage-Sidibem <mariam.sidibe@etu.univ-grenoble-alpes.fr>
2023-03-04 17:28:35 +0100

    developpement de l'api OK

--- service-pub/src/main/java/fr/uga/l3miage/library/service/BookService.java
+++ service-pub/src/main/java/fr/uga/l3miage/library/service/BookService.java
@@ -63,4 +63,7 @@ public interface BookService extends BaseService<Book, Long> {
      * @throws EntityNotFoundException if either the book or the author is not found
      */
     Book addAuthor(Long bookId, Long authorId) throws EntityNotFoundException;
+
+    static void deleteBook(Long id) {
+    }
 }
